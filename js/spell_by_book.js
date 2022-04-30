5c28264e9641c311e5dba5219f507026956704eb
AlessioVerardo <44498464+AlessioVerardo@users.noreply.github.com>
2022-04-30 20:53:22 +0200

    spell by book visualization advancement

--- js/spell_by_book.js
+++ js/spell_by_book.js
@@ -1,21 +1,44 @@
-var data = "http://localhost:8888/data/"
+function capitalizeFirstLetter(string) {
+	  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function showTooltip(d, info, tooltip){
+ 	var spellType = info['spellType']===""?"":" ("+info["spellType"]+")"
+ 	return tooltip.style("visibility", "visible")
+              .style("left", d['pageX']+10+"px")
+              .style("top", d['pageY']-80+"px")
+              .html("<span style='font-weight:bold'>"+capitalizeFirstLetter(info['spell'])+"</span>"+spellType+"<br>Book: <span style='font-style:italic;'>"+bookName[info['bookId']]+"</span><br>\"..."+info['sentence']+"...\"")
+ }
+
+
+
+var colors = {1:"rgb(75, 160, 194,0.8)", 2:"rgb(84, 173, 137,0.8)", 3:"rgb(239, 183, 71,0.8)", 4:"rgb(233, 135, 55,0.8)", 5:"rgb(217, 71, 81,0.8)", 6:"rgb(220, 123, 176,0.8)",7:"rgb(118, 117, 187,0.8)"}
+var colorsStroke = {1:"rgb(75, 160, 194,1.0)", 2:"rgb(84, 173, 137,1)", 3:"rgb(239, 183, 71,1)", 4:"rgb(233, 135, 55,1)", 5:"rgb(217, 71, 81,1)", 6:"rgb(220, 123, 176,1.0)",7:"rgb(118, 117, 187,1.0)"}
+var bookName = {1:"Philosopher's Stone", 2:"Chamber of Secrets", 3:"Prisoner of Azkaban", 4:"Goblet of Fire", 5:"Order of the Phoenix", 6:"Half-Blood Prince", 7:"Deathly Hallows"}
+
+
+var data = "http://localhost:80/data/"
 var spell_data = data+"cleaned/spellByBooke.csv"
 
 var width =  1200
-var height = 900
-var margin = {left: 90, top: 80, bottom: 50, right: 20};
+var height = 750
+var margin = {left: 40, top: 40, bottom: 40, right: 40}
 
-var svg = d3.select("#spellByBook")
+
+var svg = d3.select("#spellByBookContainer")
 			.append("svg")
-			.attr("width", width)
-			.attr("height", height)
+			.attr("width", width+margin.right +margin.left)
+			.attr("height", height+margin.top+margin.bottom)
 			.append("g")
-			.attr("transform","translate("+margin.left+","+margin.right+")")
+			.attr("transform","translate("+margin.left+","+margin.top+")")
+
+
 
 const tooltip = d3.select("body")
-              .append("div")
-              .attr("id", "tooltip")
-              .style("visibility", "hidden")
+	              .append("div")
+	              .attr("id", "tooltip")
+	              .style("visibility", "hidden")
+
 
 d3.csv(spell_data).then(data=>{
 	const counts = {}
@@ -24,53 +47,42 @@ d3.csv(spell_data).then(data=>{
 		counts[d.spell] = counts[d.spell] ? counts[d.spell] + 1 : 1;
 	})
 
-	console.log(Object.keys(counts).length)
 	var sortedCounts = Object.keys(counts).map(d=>[d,counts[d]]).sort((d1,d2)=> d2[1]-d1[1]).filter(d=>d[1]>5).map(d=>d[0])
-
 	
-	var colors = {7:"#ff0000", 1:"#db0000", 2:"#b60000", 3:"#920000", 4:"#6d0000", 5:"#490000", 6:"#240000"}
-	var bookName = {1:"Philosopher's Stone", 2:"Chamber of Secrets", 3:"Prisoner of Azkaban", 4:"Goblet of Fire", 5:"Order of the Phoenix", 6:"Half-Blood Prince", 7:"Deathly Hallows"}
-	console.log(sortedCounts.length)
+	
+	var x0 = [0,68000]
+	var y0 = sortedCounts
 
-	var yScale = d3.scaleBand()
-				    .domain(sortedCounts)
-				    .range([0, sortedCounts.length*20])
+	var yScale = d3.scalePoint()
+				    .domain(y0)
+				    .range([0, height])
 
-	var xScale = d3.scaleLinear().domain([0,68000]).range([100,1100]) 
+	var xScale = d3.scaleLinear().domain(x0).range([0,width]) 
 
 	var xAxis = d3.axisBottom().scale(xScale).tickFormat(d3.format("d"))
     var yAxis = d3.axisLeft().scale(yScale)
 
-	function zoomed() {
-	  svg.select(".x.axis").call(xAxis);
-	  svg.select(".y.axis").call(yAxis);
-	}
+	
+    var brush = d3.brushX().on("end", brushended),
+    idleTimeout,
+    idleDelay = 350;
 
-	function capitalizeFirstLetter(string) {
-	  return string.charAt(0).toUpperCase() + string.slice(1);
-	}
+	svg.append("g")
+	   .attr("class", "brush")
+	   .call(brush);
 
-    var zoom = d3.zoom()
-	    //.x(xScale)
-	    //.y(yScale)
-	    .scaleExtent([1, 10])
-	    .on("zoom", zoomed);
+  	svg.append("g")
+	   .attr("class", "axis axis--x")
+	   .attr("transform", "translate("+margin.left+", "+(-margin.left)+")")
+	   .call(xAxis);
 
+	svg.append("g")
+	   .attr("class", "axis axis--y") // assign an axis class
+	   .attr("transform", "translate("+ (margin.left-10)+", 0)")//"+margin.top+")")
+	   .call(yAxis);
 
-
-    svg.append("g")
-       .attr("transform", "translate(0, "+ (height - margin.bottom) + ")")
-       .attr("id", "x-axis")
-       .call(xAxis)
-
-    svg.append("g")
-       .attr("transform", "translate("+ (margin.left)+", 0)")
-       .attr("id", "y-axis")
-       .call(yAxis)
-
-    console.log(sortedCounts)
     data = data.filter(x => sortedCounts.includes(x.spell))
-    console.log(data)
+
 	svg.selectAll("scatterPoints")
 		.data(data)
 		.enter()
@@ -79,22 +91,48 @@ d3.csv(spell_data).then(data=>{
 		.attr("cy", d=> yScale(d.spell))
 		.attr("r", 10)
         .style("fill", d => colors[d.bookId])
-        .style("opacity", 0.8)
+        .style("stroke-width", 3)
+        .style("stroke",d=>colorsStroke[d.bookId])
         .on("click", d =>{
-           showTooltip(d, d["srcElement"]["__data__"])
+           showTooltip(d, d["srcElement"]["__data__"], tooltip)
        	})
        	.on("mouseover", d =>{
-           showTooltip(d, d["srcElement"]["__data__"])
+           showTooltip(d, d["srcElement"]["__data__"], tooltip)
        	})
      	.on("mouseout", function(){
            tooltip.style("visibility", "hidden")
        })
 
-     function showTooltip(d, info){
-     	var spellType = info['spellType']===""?"":" ("+info["spellType"]+")"
-     	return tooltip.style("visibility", "visible")
-                  .style("left", d['pageX']+10+"px")
-                  .style("top", d['pageY']-80+"px")
-                  .html("<span style='font-weight:bold'>"+capitalizeFirstLetter(info['spell'])+"</span>"+spellType+"<br>Book: <span style='font-style:italic;'>"+bookName[info['bookId']]+"</span><br>\"..."+info['sentence']+"...\"")
-     }
+
+function catAxis(){
+
+}
+
+ function brushended(e) {
+    var s = e.selection;
+    if (!s) {
+        if (!idleTimeout) return idleTimeout = setTimeout(idled, idleDelay);
+            xScale.domain(x0);
+            yScale.domain(y0)
+    } else {
+        xScale.domain([s[0], s[1]].map(xScale.invert, xScale))
+        svg.select(".brush").call(brush.move, null);
+    }
+    zoom()
+}
+
+function idled() {
+    idleTimeout = null;
+}
+
+function zoom() {
+    var t = svg.transition().duration(750);
+    svg.select(".axis--x").transition(t).call(xAxis);
+    svg.select(".axis--y").transition(t).call(yAxis);
+    svg.selectAll("circle").transition(t)
+    .attr("cx", d=> xScale(d.lineNb))
+    .attr("cy", d=> yScale(d.spell))
+}
+    
+
 })
