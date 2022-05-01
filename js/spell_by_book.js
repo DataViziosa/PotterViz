bf8eeb7da9a213b79ed4aa2d913b8d67ca8edb97
AlessioVerardo <44498464+AlessioVerardo@users.noreply.github.com>
2022-05-01 09:47:49 +0200

    add comment on the spell by book visualization

--- js/spell_by_book.js
+++ js/spell_by_book.js
@@ -1,7 +1,19 @@
+/**
+ * Return the string with the first letter in upper case
+ * @param string the string to capitalize the first letter. 
+ * 
+ */
 function capitalizeFirstLetter(string) {
 	  return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+/**
+ * Display the tooltip over the selected point
+ * @param d the information of the selection event
+ * @param info the information of the spell casted
+ * @param tooltip the tooltip object
+ * 
+ */
 function showTooltip(d, info, tooltip){
  	var spellType = info['spellType']===""?"":" ("+info["spellType"]+")"
  	return tooltip.style("visibility", "visible")
@@ -11,12 +23,13 @@ function showTooltip(d, info, tooltip){
  }
 
 
-
+// Define useful dictionnaries
 var colors = {1:"rgb(75, 160, 194,0.8)", 2:"rgb(84, 173, 137,0.8)", 3:"rgb(239, 183, 71,0.8)", 4:"rgb(233, 135, 55,0.8)", 5:"rgb(217, 71, 81,0.8)", 6:"rgb(220, 123, 176,0.8)",7:"rgb(118, 117, 187,0.8)"}
 var colorsStroke = {1:"rgb(75, 160, 194,1.0)", 2:"rgb(84, 173, 137,1)", 3:"rgb(239, 183, 71,1)", 4:"rgb(233, 135, 55,1)", 5:"rgb(217, 71, 81,1)", 6:"rgb(220, 123, 176,1.0)",7:"rgb(118, 117, 187,1.0)"}
 var bookName = {1:"Philosopher's Stone", 2:"Chamber of Secrets", 3:"Prisoner of Azkaban", 4:"Goblet of Fire", 5:"Order of the Phoenix", 6:"Half-Blood Prince", 7:"Deathly Hallows"}
 
 
+// define over all constants
 var data = "http://localhost:80/data/"
 var spell_data = data+"cleaned/spellByBooke.csv"
 
@@ -25,6 +38,7 @@ var height = 750
 var margin = {left: 40, top: 40, bottom: 40, right: 40}
 
 
+// add the svg object to html
 var svg = d3.select("#spellByBookContainer")
 			.append("svg")
 			.attr("width", width+margin.right +margin.left)
@@ -33,7 +47,7 @@ var svg = d3.select("#spellByBookContainer")
 			.attr("transform","translate("+margin.left+","+margin.top+")")
 
 
-
+// create the tooltip div 
 const tooltip = d3.select("body")
 	              .append("div")
 	              .attr("id", "tooltip")
@@ -43,22 +57,30 @@ const tooltip = d3.select("body")
 d3.csv(spell_data).then(data=>{
 	const counts = {}
 
+	// create a dictionnry of count
 	data.forEach(d=>{
 		counts[d.spell] = counts[d.spell] ? counts[d.spell] + 1 : 1;
 	})
 
-	var sortedCounts = Object.keys(counts).map(d=>[d,counts[d]]).sort((d1,d2)=> d2[1]-d1[1]).filter(d=>d[1]>5).map(d=>d[0])
+	var sortedCounts = Object.keys(counts) // get the keys in the dictionnaries
+							.map(d=>[d,counts[d]]) // map the keys to itself and its count
+							.sort((d1,d2)=> d2[1]-d1[1]) // sort by count
+							.filter(d=>d[1]>5) // filter out the spell with less than 5 occurences
+							.map(d=>d[0]) // get the ke<
 	
 	
-	var x0 = [0,68000]
+	var x0 = [0,68000] //@TODO should be replaced by the maximal line count
 	var y0 = sortedCounts
 
+	// y scale is ordinal 
 	var yScale = d3.scalePoint()
 				    .domain(y0)
 				    .range([0, height])
 
+    // xscale is linear in the number of lines 
 	var xScale = d3.scaleLinear().domain(x0).range([0,width]) 
 
+	// Create axis from the scale
 	var xAxis = d3.axisBottom().scale(xScale).tickFormat(d3.format("d"))
     var yAxis = d3.axisLeft().scale(yScale)
 
@@ -66,6 +88,7 @@ d3.csv(spell_data).then(data=>{
     var brush = d3.brushX().on("end", brushended),
     idleTimeout,
     idleDelay = 350;
+
 
 	svg.append("g")
 	   .attr("class", "brush")
@@ -83,38 +106,41 @@ d3.csv(spell_data).then(data=>{
 
     data = data.filter(x => sortedCounts.includes(x.spell))
 
+    // Create all the points
 	svg.selectAll("scatterPoints")
 		.data(data)
 		.enter()
 		.append("circle")
-		.attr("cx", d=> xScale(d.lineNb))
-		.attr("cy", d=> yScale(d.spell))
+		.attr("cx", d=> xScale(d.lineNb)) // x psition depends on the line in of the book
+		.attr("cy", d=> yScale(d.spell)) // y-position is depending on the spell
 		.attr("r", 10)
-        .style("fill", d => colors[d.bookId])
+        .style("fill", d => colors[d.bookId]) // color depends on the book in which this spell is launched 
         .style("stroke-width", 3)
         .style("stroke",d=>colorsStroke[d.bookId])
         .on("click", d =>{
-           showTooltip(d, d["srcElement"]["__data__"], tooltip)
+           showTooltip(d, d["srcElement"]["__data__"], tooltip) // show a superposed div with the books in which it appears and the context (the sentence)
        	})
        	.on("mouseover", d =>{
-           showTooltip(d, d["srcElement"]["__data__"], tooltip)
+           showTooltip(d, d["srcElement"]["__data__"], tooltip) // show a superposed div with the books in which it appears and the context (the sentence)
        	})
      	.on("mouseout", function(){
-           tooltip.style("visibility", "hidden")
+           tooltip.style("visibility", "hidden") // remove the div
        })
 
-
-function catAxis(){
-
-}
-
+ /**
+  * Zoom in the plot depending on the selection
+  * @param e the brush selection
+  */
  function brushended(e) {
-    var s = e.selection;
+ 	// get the selection boundary
+    var s = e.selection
     if (!s) {
-        if (!idleTimeout) return idleTimeout = setTimeout(idled, idleDelay);
-            xScale.domain(x0);
+    	// if the selection is empty (e.g. when we double click for instance), reset to default value
+        if (!idleTimeout) return idleTimeout = setTimeout(idled, idleDelay)
+            xScale.domain(x0)
             yScale.domain(y0)
     } else {
+    	// get the scale back 
         xScale.domain([s[0], s[1]].map(xScale.invert, xScale))
         svg.select(".brush").call(brush.move, null);
     }
