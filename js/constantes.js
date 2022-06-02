82a198cddd17535902108afa89df07f48858ed69
AlessioVerardo <44498464+AlessioVerardo@users.noreply.github.com>
2022-06-02 23:15:37 +0200

    sankey beginning and pie chart

--- js/constantes.js
+++ js/constantes.js
@@ -4,3 +4,5 @@ let data_path = base_path + "data/"
 let cleaned_path = data_path+ "cleaned/"
 let char_images = data_path+"images/characters/"
 let ext_img = ".png"
+
+let color_chart = ["#1AC3A6", "#34CF7A", "#41A1E1", "#A667BF", "#F2C500","#EA8B1D", "#EB5D49", "#475B6F", "#ECF0F1", "#9FAEAF"]
--- js/spells.js
+++ js/spells.js
@@ -1,28 +1,25 @@
 google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(drawChart);
+google.charts.setOnLoadCallback(drawPieChart)
 
-function drawChart() {
-
-var data = google.visualization.arrayToDataTable([
-    ['Spell', 'Percentage'],
-    ['Charm',     157],
-    ['Healing Spell',      3],
-    ['Conjuration',      7],
-
-
-    [' Curse', 25],
-    [' Hex', 21],
-    ['Transfiguration',    26],
-    ['Other',  14]
-]);
-
-var options = {
-    title: 'Repartition of Spell types',
-    backgroundColor: 'transparent'
-};
-
-var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+function drawPieChart() {
+    var data = google.visualization.arrayToDataTable([
+        ['Spell', 'Percentage'],
+        ['Charm',     157],
+        ['Healing Spell',      3],
+        ['Conjuration',      7],
 
 
-chart.draw(data, options);
+        [' Curse', 25],
+        [' Hex', 21],
+        ['Transfiguration',    26],
+        ['Other',  14]
+    ])
+
+    var options = {
+        title: 'Repartition of Spell types',
+        backgroundColor: 'transparent'
+    }
+
+    var chart = new google.visualization.PieChart(document.getElementById('piechart'))
+    chart.draw(data, options)
 }
